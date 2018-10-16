ad947fd1b3cee2aef1ca5d0c03c2736ab370d44e
Team Ninja (Emanuele) <emanuele.malfara@trovaprezzi.it>
2018-10-16 14:15:05 +0200

    greeeeen

--- exercise.rb
+++ exercise.rb
@@ -1,5 +1,33 @@
 class Exercise
   def find(matrix)
-    0
+    @matrix = matrix
+    max_area=0
+    0.upto(matrix.size-1) do |row|
+      0.upto(matrix[row].size-1) do |col|
+        max_area=[find_max(row, col), max_area].max
+      end
+    end
+    max_area
+  end
+
+  def find_max(start_r, start_c)
+    [
+      area_for_range(start_r, start_r , start_c, start_c + 1),
+      area_for_range(start_r, start_r + 1, start_c, start_c ),
+      @matrix[start_r][start_c] == 1 ? 1 : 0
+    ].max
+  end
+
+  def area_for_range(row, max_row,col, max_col)
+    return 0 if max_row >= @matrix.size || max_col >= @matrix[0].size
+    sum = 0
+    row.upto(max_row) do |r|
+      col.upto(max_col) do |c|
+        return 0 if (@matrix[r][c] == 0)
+        sum += 1
+      end
+    end
+    return [sum, area_for_range(row, max_row , col, max_col + 1),
+     area_for_range(row, max_row +1 , col, max_col )].max
   end
 end
--- test_exercise.rb
+++ test_exercise.rb
@@ -2,10 +2,58 @@ require_relative "exercise"
 require "test/unit"
 
 class TestExercise < Test::Unit::TestCase
-  test 'base example ' do
-    assert_equal 4, Exercise.new.find([[1, 0, 0, 0],
-     [1, 0, 1, 1],
-     [1, 0, 1, 1],
-     [0, 1, 0, 0]])
+  test 'vertical ' do
+    assert_equal 4, Exercise.new.find([
+      [1, 0, 0, 0],
+      [1, 0, 1, 1],
+      [1, 0, 1, 1],
+      [0, 1, 0, 0]])
+  end
+
+  test 'simple square ' do
+    assert_equal 4, Exercise.new.find([
+      [1, 0, 0, 0],
+      [1, 0, 1, 1],
+      [1, 0, 1, 1],
+      [0, 1, 0, 0]])
+  end
+
+  test 'horizontal ' do
+    assert_equal 3, Exercise.new.find([
+      [0, 0, 0, 0],
+      [1, 0, 1, 1],
+      [1, 0, 0, 0],
+      [0, 1, 1, 1]])
+  end
+
+  test 'horizontal rectangle' do
+    assert_equal 8, Exercise.new.find([
+      [0, 1, 1, 0],
+      [1, 1, 1, 1],
+      [1, 1, 1, 1],
+      [0, 1, 0, 0]])
+  end
+
+  test 'vertical rectangle' do
+    assert_equal 6, Exercise.new.find([
+      [0, 1, 1, 0],
+      [1, 1, 1, 1],
+      [0, 1, 1, 0],
+      [0, 1, 0, 0]])
+  end
+
+  test 'inner square' do
+    assert_equal 9, Exercise.new.find([
+      [1, 1, 0, 1, 0],
+      [1, 1, 1, 1, 1],
+      [0, 1, 1, 1, 0],
+      [1, 1, 1, 1, 1],
+      [1, 1, 0, 1, 1]])
+  end
+
+  test 'one dot' do
+    assert_equal 1, Exercise.new.find([
+      [1, 0],
+      [0, 1]])
   end
 end
